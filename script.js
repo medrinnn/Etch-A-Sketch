577b1e4e94995f959caa3b5a389f4c27bd382d00
medrinnn <182343377+medrinnn@users.noreply.github.com>
2025-10-29 22:10:24 +0000

    add grid + random color logic and fix a bug

--- script.js
+++ script.js
@@ -2,25 +2,29 @@ const grid = document.querySelector("#grid");
 const gridsizeBtn = document.querySelector("#gridsizeBtn");
 const clearBtn = document.querySelector("#clearBtn");
 
-gridsizeBtn.addEventListener("click", (() => {
-    gridsize = Number(window.prompt("Type The Grid Size"));
+gridsizeBtn.addEventListener("click", () => {
+    const gridsize = Number(window.prompt("Type The Grid Size"));
     if (gridsize >= 5 && gridsize <= 50) {
         grid.innerHTML = "";
-        for (let i = 0; i < gridsize; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.onmouseover = function() {
-            red = Math.floor(Math.random() * 255);
-            blue = Math.floor(Math.random() * 255);
-            green = Math.floor(Math.random() * 255);
-            this.style.backgroundColor = 'rgb(' + red + ',' + blue + ',' + green + ')';
-        }
-        grid.appendChild(square);
+        grid.style.display = "grid";
+        grid.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`;
+
+        for (let i = 0; i < gridsize * gridsize; i++) { 
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.onmouseover = function() {
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+                this.style.backgroundColor = `rgb(${red},${green},${blue})`;
+            }
+            grid.appendChild(square);
         }
     } else {
         alert("Error: Invalid Value");
     }
-}));
+});
 
 clearBtn.addEventListener("click", () => {
     const squares = document.querySelectorAll(".square");
@@ -28,5 +32,3 @@ clearBtn.addEventListener("click", () => {
         square.style.backgroundColor = "white";
     });
 });
-
-
